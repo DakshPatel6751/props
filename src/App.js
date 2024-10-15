fd905f9bd9c9c860e2ea26fc82f3c53160463ab9
Daksh Patel <157096650+DakshPatel6751@users.noreply.github.com>
2024-10-15 15:42:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Image  from './image';
+
+function App() {
+  return (
+    <div className="App" style={{}}>
+
+   <Image src = "https://plus.unsplash.com/premium_photo-1728731295527-b8492d105771?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"  height= {300} width = {300} title={"Hello ALL"}>
+
+   </Image>
+
+
+   
+  
+   <Image src = "https://images.unsplash.com/photo-1728069002339-8d04bd46940f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" height= {300} width = {300} title={"HELLO"}></Image>
+   
+   <Image src = "https://images.unsplash.com/photo-1728943492981-be3e94e4d551?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8" height= {300} width = {300} title={"GOOD"}></Image>
+   
+   <Image src = "https://images.unsplash.com/photo-1727873817701-8cd132afbb01?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D" height= {300} width = {300} title={"NEW"}></Image>
+  
+   <Image src =  "https://images.unsplash.com/photo-1728716858431-001a104dd639?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D" height= {300} width = {300} title={"WELCOME"}></Image>
+
+    </div>
+  );
+}
+
+export default App;
